fabfbaed81f7a71b8fe7a8ad5c464d2577287625
bocheng24 <jeffcgp@hotmail.com>
2021-12-23 16:25:31 +0800

    change images url

--- src/components/Header.js
+++ src/components/Header.js
@@ -44,7 +44,7 @@ function Header() {
 
             <Profile>
                 <img 
-                src="https://images.unsplash.com/photo-1527980965255-d3b416303d12?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" 
+                src="https://images.unsplash.com/photo-1562124638-724e13052daf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80" 
                 alt="profileimg" />
             </Profile>
         </Wrapper>
