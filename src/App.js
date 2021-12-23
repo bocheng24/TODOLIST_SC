06c9886eb4dd029baac0cba5df33040d4d698478
bocheng24 <jeffcgp@hotmail.com>
2021-12-23 22:57:01 +0800

    styled the main header and layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,60 @@
-import './App.css';
+import styled from 'styled-components';
 
+// Components
 import Header from './components/Header';
+
+const Wrapper = styled.div`
+  background-color: #181818;
+  min-height: 100vh;
+  min-width: 100vw;
+  color: #fff;
+`;
+
+const Main = styled.div`
+  display: flex;
+`;
+
+const MainContent = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+`;
+  
+const TodoContent = styled.div`
+  max-width: 750px;
+  width: 100%;
+`;
+
+const Title = styled.div`
+  margin: 45px 0;
+  font-size: 32px;
+  font-weight: 700;
+  letter-spacing: 0.35px;
+`;
+
+const Greeting = styled.div`
+  margin-bottom: 25px;
+  font-size: 36px;
+  font-weight: 800;
+  letter-spacing: 0.85px;
+`;
 
 function App() {
   
   return (
-    <div>
+    <Wrapper>
       <Header />
-      <h2>To do list app</h2>
-    </div>
+
+      <Main>
+        <MainContent>
+          <TodoContent>
+            <Title>Dashboard</Title>
+            <Greeting>Good Day, Joe!</Greeting>
+            {[<h2>Item 1</h2>, <h2>Item 2</h2>]}
+          </TodoContent>
+        </MainContent>
+      </Main>
+    </Wrapper>
   );
 }
 
